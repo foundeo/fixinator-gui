018e17c15acc1b4a7daa0321cc81e46719836fca
Pete Freitag <pete@foundeo.com>
2020-06-29 16:40:21 -0400

    Update init script

--- build/init.sh
+++ build/init.sh
@@ -5,20 +5,26 @@ cd ../
 
 npm install
 
+npm audit
+
 cd resources/cfml/assets/
 
 npm install
 
-cd resources/cfml/
+npm audit
 
-box install
-
-cd ../../build/
+cd ../../../build/
 
 ./clean.sh
 
 cd ../
+
 #download box.jar
 
 curl -o ./resources/box.jar https://s3.amazonaws.com/downloads.ortussolutions.com/ortussolutions/commandbox/5.1.1/box.jar
+
+cd resources/cfml/
+
+#box install
+java -jar ../box.jar install
 
